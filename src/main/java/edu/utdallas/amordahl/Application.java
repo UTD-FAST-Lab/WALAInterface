682faca507e8cad5e3e5c1308c3f3b4b9c6745ce
Austin Mordahl <austin.mordahl@utdallas.edu>
2024-09-17 18:15:07 -0500

    Update to accept directories

--- src/main/java/edu/utdallas/amordahl/Application.java
+++ src/main/java/edu/utdallas/amordahl/Application.java
@@ -21,208 +21,238 @@ import picocli.CommandLine;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Application {
 
-    // private static final Logger logger =
-    // LoggerFactory.getLogger(Application.class);
+	// private static final Logger logger =
+	// LoggerFactory.getLogger(Application.class);
 
-    private static CommandLineOptions clo;
+	private static CommandLineOptions clo;
 
-    public static void main(final String[] args)
-            throws WalaException, CallGraphBuilderCancelException, IOException {
-        // Initialize command line and print help if requested.
-        Application.clo = new CommandLineOptions();
-        new CommandLine(Application.clo).parseArgs(args);
-        if (Application.clo.usageHelpRequested) {
-            CommandLine.usage(new CommandLineOptions(), System.out);
-            return;
-        }
-        System.out.println("WALA Property java_runtime_dir is " + Arrays.toString(WalaProperties.getJ2SEJarFiles()));
+	public static void main(final String[] args) throws WalaException, CallGraphBuilderCancelException, IOException {
+		// Initialize command line and print help if requested.
+		Application.clo = new CommandLineOptions();
+		new CommandLine(Application.clo).parseArgs(args);
+		if (Application.clo.usageHelpRequested) {
+			CommandLine.usage(new CommandLineOptions(), System.out);
+			return;
+		}
 
-        // Build call graph.
-        final CallGraph cg = new Application().makeCallGraph(Application.clo);
+		System.out.println("WALA Property (change in src/main/resources/wala.properties) java_runtime_dir is "
+				+ Arrays.toString(WalaProperties.getJ2SEJarFiles()));
 
-        // Print to output.
-        List<Map<String, String>> callGraph = new LinkedList<Map<String, String>>();
-        for (final CGNode callGraphNode : cg) {
-            final Iterator<CallSiteReference> callSiteIterator = callGraphNode.iterateCallSites();
-            while (callSiteIterator.hasNext()) {
-                final CallSiteReference callSite = callSiteIterator.next();
-                for (final CGNode target : cg.getPossibleTargets(callGraphNode, callSite)) {
-                    final Map<String, String> callGraphEdge = new HashMap<String, String>();
-                    callGraphEdge.put("caller", callGraphNode.getMethod().getSignature());
-                    callGraphEdge.put("callInstruction", callSite.toString());
-                    callGraphEdge.put("actualTarget", target.getMethod().getSignature());
-                    List<String> contexts = new LinkedList<String>();
-                    if (Application.clo.callGraphBuilder == CallGraphBuilders.NCFA) {
-                        // Then we know the context is a CallString type.
-                        CallString context = (CallString) target.getContext().get(CallStringContextSelector.CALL_STRING);
-                        if (context != null) { // if it's null that means that it was an Everywhere context and we don't have to worry about it.
-                            for (int i = 0; i < context.getMethods().length; i++) {
-                                IMethod methodRef = context.getMethods()[i];
-                                CallSiteReference csr = context.getCallSiteRefs()[i];
-                                int realLineNumber = methodRef.getLineNumber(csr.getProgramCounter());
-                                String contextString = methodRef.getDeclaringClass().getName().toString()
-                                        .substring(1).replace("/", ".") + "." +
-                                        methodRef.getName() + methodRef.getDescriptor() + ":" +
-                                        realLineNumber;
-                                contexts.add(contextString);
-                            }
-                        }
-                    } else if (Application.clo.callGraphBuilder == CallGraphBuilders.NOBJ) {
-                        AllocationString context = (AllocationString) target.getContext().get(nObjContextSelector.ALLOCATION_STRING_KEY);
-                        if (context != null) {
-                            for (int i = 0; i < context.getAllocationSites().length; i++) {
-                                AllocationSite as = context.getAllocationSites()[i];
-                                String contextString = as.getMethod().getDeclaringClass().getName().toString()
-                                        .substring(1).replace("/",".") + "." +
-                                        as.getMethod().getName() + as.getMethod().getDescriptor() + ":" +
-                                        as.getMethod().getLineNumber(as.getSite().getProgramCounter());
-                                contexts.add(contextString);
-                            }
-                        }
-                    }
-                    callGraphEdge.put("contexts", contexts.toString());
-                    //System.out.println("Context list is " + contexts.toString());
-                    callGraph.add(callGraphEdge);
-                }
-            }
-        }
-        // Break up call graph into multiple files, in order to prevent really big files.
-        int iteration = 0;
-        int intervalSize = 1000000;
-        if (callGraph.size() < intervalSize) {
-            writeChunkToFile(callGraph, Application.clo.callgraphOutput.toString());
-            System.out.println("Wrote callgraph to " + Application.clo.callgraphOutput.toString());
-        } else {
-            System.out.println("Writing in chunks of " + intervalSize + " in order to prevent huge files.");
-            while (callGraph.size() >= intervalSize) {
-                List<Map<String, String>> chunk = callGraph.subList(0, intervalSize);
-                callGraph = callGraph.subList(intervalSize, callGraph.size());
-                String file = Application.clo.callgraphOutput.toString() + iteration;
-                writeChunkToFile(chunk, Application.clo.callgraphOutput.toString() + iteration);
-                System.out.println("Wrote chunk of callgraph to " + file);
-                iteration += 1;
-            }
-        }
-    }
+		// Build call graph.
+		final CallGraph cg = new Application().makeCallGraph(Application.clo);
 
-    private static void writeChunkToFile(List<Map<String, String>> chunk, String fileName) throws IOException {
-        final FileWriter fw = new FileWriter(fileName);
-        ObjectMapper om = new ObjectMapper();
-        fw.write("[");
-        Iterator<Map<String, String>> callGraphIterator = chunk.iterator();
-        while (callGraphIterator.hasNext()) {
-            fw.write(om.writeValueAsString(callGraphIterator.next()));
-            if (callGraphIterator.hasNext()) fw.write(",");
-        }
-        fw.write("]");
-        fw.close();
-    }
+		// Print to output.
+		List<Map<String, String>> callGraph = new LinkedList<Map<String, String>>();
+		for (final CGNode callGraphNode : cg) {
+			final Iterator<CallSiteReference> callSiteIterator = callGraphNode.iterateCallSites();
+			while (callSiteIterator.hasNext()) {
+				final CallSiteReference callSite = callSiteIterator.next();
+				for (final CGNode target : cg.getPossibleTargets(callGraphNode, callSite)) {
+					final Map<String, String> callGraphEdge = new HashMap<String, String>();
+					callGraphEdge.put("caller", callGraphNode.getMethod().getSignature());
+					callGraphEdge.put("callInstruction", callSite.toString());
+					callGraphEdge.put("actualTarget", target.getMethod().getSignature());
+					List<String> contexts = new LinkedList<String>();
+					if (Application.clo.callGraphBuilder == CallGraphBuilders.NCFA) {
+						// Then we know the context is a CallString type.
+						CallString context = (CallString) target.getContext()
+								.get(CallStringContextSelector.CALL_STRING);
+						if (context != null) { // if it's null that means that it was an Everywhere context and we don't
+												// have to worry about it.
+							for (int i = 0; i < context.getMethods().length; i++) {
+								IMethod methodRef = context.getMethods()[i];
+								CallSiteReference csr = context.getCallSiteRefs()[i];
+								int realLineNumber = methodRef.getLineNumber(csr.getProgramCounter());
+								String contextString = methodRef.getDeclaringClass().getName().toString().substring(1)
+										.replace("/", ".") + "." + methodRef.getName() + methodRef.getDescriptor() + ":"
+										+ realLineNumber;
+								contexts.add(contextString);
+							}
+						}
+					} else if (Application.clo.callGraphBuilder == CallGraphBuilders.NOBJ) {
+						AllocationString context = (AllocationString) target.getContext()
+								.get(nObjContextSelector.ALLOCATION_STRING_KEY);
+						if (context != null) {
+							for (int i = 0; i < context.getAllocationSites().length; i++) {
+								AllocationSite as = context.getAllocationSites()[i];
+								String contextString = as.getMethod().getDeclaringClass().getName().toString()
+										.substring(1).replace("/", ".") + "." + as.getMethod().getName()
+										+ as.getMethod().getDescriptor() + ":"
+										+ as.getMethod().getLineNumber(as.getSite().getProgramCounter());
+								contexts.add(contextString);
+							}
+						}
+					}
+					callGraphEdge.put("contexts", contexts.toString());
+					// System.out.println("Context list is " + contexts.toString());
+					callGraph.add(callGraphEdge);
+				}
+			}
+		}
 
-    public CallGraph makeCallGraph(final CommandLineOptions clo)
-            throws ClassHierarchyException, IOException, CallGraphBuilderCancelException {
-        final AnalysisScope scope = AnalysisScopeReader.instance.makeJavaBinaryAnalysisScope(clo.appJar,
-                new File("resources/exclusions.txt"));
+		// Write the call graph
+		writeChunkToFile(callGraph, Application.clo.callgraphOutput.toString());
+		System.out.println("Wrote callgraph to " + Application.clo.callgraphOutput.toString());
+	}
 
-        final ClassHierarchy cha = ClassHierarchyFactory.make(scope);
+	private static void writeChunkToFile(List<Map<String, String>> chunk, String fileName) throws IOException {
+		final FileWriter fw = new FileWriter(fileName);
+		ObjectMapper om = new ObjectMapper();
+		fw.write("[");
+		Iterator<Map<String, String>> callGraphIterator = chunk.iterator();
+		while (callGraphIterator.hasNext()) {
+			fw.write(om.writeValueAsString(callGraphIterator.next()));
+			if (callGraphIterator.hasNext())
+				fw.write(",");
+		}
+		fw.write("]");
+		fw.close();
+	}
 
-        final Iterable<Entrypoint> entrypoints = com.ibm.wala.ipa.callgraph.impl.Util.makeMainEntrypoints(cha);
-        final AnalysisOptions options = new AnalysisOptions(scope, entrypoints);
-        options.setReflectionOptions(clo.reflection);
-        options.setHandleStaticInit(!clo.disableHandleStaticInit);
-        options.setUseConstantSpecificKeys(clo.useConstantSpecificKeys);
-        options.setUseStacksForLexicalScoping(clo.useStacksForLexicalScoping);
-        options.setUseLexicalScopingForGlobals(clo.useLexicalScopingForGlobals);
-        options.setMaxNumberOfNodes(clo.maxNumberOfNodes);
-        options.setHandleZeroLengthArray(!clo.disableHandleZeroLengthArray);
+	public CallGraph makeCallGraph(final CommandLineOptions clo)
+			throws ClassHierarchyException, IOException, CallGraphBuilderCancelException {
 
-        // //
-        // build the call graph
-        // //
-        CallGraphBuilder<InstanceKey> builder;
-        switch (clo.callGraphBuilder) {
-            case NCFA:
-                builder = Util.makeNCFABuilder(clo.sensitivity, options, new AnalysisCacheImpl(), cha);
-                break;
-            case NOBJ:
-                builder = Util.makeNObjBuilder(clo.sensitivity, options, new AnalysisCacheImpl(), cha);
-                break;
-            case VANILLA_NCFA:
-                builder = Util.makeVanillaNCFABuilder(clo.sensitivity, options, new AnalysisCacheImpl(), cha);
-                break;
-            case VANILLA_NOBJ:
-                builder = Util.makeVanillaNObjBuilder(clo.sensitivity, options, new AnalysisCacheImpl(), cha);
-                break;
-            case RTA:
-                builder = Util.makeRTABuilder(options, new AnalysisCacheImpl(), cha);
-                break;
-            case ZERO_CFA:
-                builder = Util.makeZeroCFABuilder(Language.JAVA, options, new AnalysisCacheImpl(), cha);
-                break;
-            case ZEROONE_CFA:
-                builder = Util.makeZeroOneCFABuilder(Language.JAVA, options, new AnalysisCacheImpl(), cha);
-                break;
-            case VANILLA_ZEROONECFA:
-                builder = Util.makeVanillaZeroOneCFABuilder(Language.JAVA, options, new AnalysisCacheImpl(), cha);
-                break;
-            case ZEROONE_CONTAINER_CFA:
-                builder = Util.makeZeroOneContainerCFABuilder(options, new AnalysisCacheImpl(), cha);
-                break;
-            case VANILLA_ZEROONE_CONTAINER_CFA:
-                builder = Util.makeVanillaZeroOneContainerCFABuilder(options, new AnalysisCacheImpl(), cha);
-                break;
-            case ZERO_CONTAINER_CFA:
-                builder = Util.makeZeroContainerCFABuilder(options, new AnalysisCacheImpl(), cha);
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid call graph algorithm.");
-        }
-        final long startTime = System.currentTimeMillis();
+		LinkOption[] searchSettings = new LinkOption[] {};
+		StringBuilder jars = new StringBuilder();
+		Iterator<String> jarIt = clo.jars.iterator();
+		while (jarIt.hasNext()) {
+			Path p = Paths.get(jarIt.next());
+			if (!Files.exists(p, searchSettings)) {
+				throw new RuntimeException("Given path does not exist.");
+			} else if (Files.isRegularFile(p, searchSettings)) {
+				jars.append(p.toString());
+				if (jarIt.hasNext()) {
+					jars.append(File.pathSeparator);
+				}
+			} else if (Files.isDirectory(p, searchSettings)) {
+				if (!Files.list(p).findFirst().isPresent()) {
+					throw new RuntimeException("Empty directory provided.");
+				}
+				try (Stream<Path> paths = Files.list(p)) {
+					// Filter the stream to only include .jar files
+					String jarFilesList = paths.filter(Files::isRegularFile)
+							.filter(path -> path.toString().endsWith(".jar")).map(Path::toString) // Convert Path to
+																									// String
+							.collect(Collectors.joining(File.pathSeparator)); // Join the file paths with a colon
+					jars.append(jarFilesList);
+					if (jarIt.hasNext()) {
+						jars.append(File.pathSeparator);
+					}
+				}
+			}
+		}
 
-        final MonitorUtil.IProgressMonitor pm = new MonitorUtil.IProgressMonitor() {
-            private boolean cancelled;
+		System.out.println("Jars to analyze are " + jars.toString());
 
-            @Override
-            public void beginTask(final String s, final int i) {
+		final AnalysisScope scope = AnalysisScopeReader.instance.makeJavaBinaryAnalysisScope(jars.toString(),
+				new File("resources/exclusions.txt"));
 
-            }
+		final ClassHierarchy cha = ClassHierarchyFactory.make(scope);
 
-            @Override
-            public void subTask(final String s) {
+		final Iterable<Entrypoint> entrypoints = com.ibm.wala.ipa.callgraph.impl.Util.makeMainEntrypoints(cha);
+		final AnalysisOptions options = new AnalysisOptions(scope, entrypoints);
+		options.setReflectionOptions(clo.reflection);
+		options.setHandleStaticInit(!clo.disableHandleStaticInit);
+		options.setUseConstantSpecificKeys(clo.useConstantSpecificKeys);
+		options.setUseStacksForLexicalScoping(clo.useStacksForLexicalScoping);
+		options.setUseLexicalScopingForGlobals(clo.useLexicalScopingForGlobals);
+		options.setMaxNumberOfNodes(clo.maxNumberOfNodes);
+		options.setHandleZeroLengthArray(!clo.disableHandleZeroLengthArray);
 
-            }
+		// //
+		// build the call graph
+		// //
+		CallGraphBuilder<InstanceKey> builder;
+		switch (clo.callGraphBuilder) {
+		case NCFA:
+			builder = Util.makeNCFABuilder(clo.sensitivity, options, new AnalysisCacheImpl(), cha);
+			break;
+		case NOBJ:
+			builder = Util.makeNObjBuilder(clo.sensitivity, options, new AnalysisCacheImpl(), cha);
+			break;
+		case VANILLA_NCFA:
+			builder = Util.makeVanillaNCFABuilder(clo.sensitivity, options, new AnalysisCacheImpl(), cha);
+			break;
+		case VANILLA_NOBJ:
+			builder = Util.makeVanillaNObjBuilder(clo.sensitivity, options, new AnalysisCacheImpl(), cha);
+			break;
+		case RTA:
+			builder = Util.makeRTABuilder(options, new AnalysisCacheImpl(), cha);
+			break;
+		case ZERO_CFA:
+			builder = Util.makeZeroCFABuilder(Language.JAVA, options, new AnalysisCacheImpl(), cha);
+			break;
+		case ZEROONE_CFA:
+			builder = Util.makeZeroOneCFABuilder(Language.JAVA, options, new AnalysisCacheImpl(), cha);
+			break;
+		case VANILLA_ZEROONECFA:
+			builder = Util.makeVanillaZeroOneCFABuilder(Language.JAVA, options, new AnalysisCacheImpl(), cha);
+			break;
+		case ZEROONE_CONTAINER_CFA:
+			builder = Util.makeZeroOneContainerCFABuilder(options, new AnalysisCacheImpl(), cha);
+			break;
+		case VANILLA_ZEROONE_CONTAINER_CFA:
+			builder = Util.makeVanillaZeroOneContainerCFABuilder(options, new AnalysisCacheImpl(), cha);
+			break;
+		case ZERO_CONTAINER_CFA:
+			builder = Util.makeZeroContainerCFABuilder(options, new AnalysisCacheImpl(), cha);
+			break;
+		default:
+			throw new IllegalArgumentException("Invalid call graph algorithm.");
+		}
+		final long startTime = System.currentTimeMillis();
 
-            @Override
-            public void cancel() {
-                this.cancelled = true;
-            }
+		final MonitorUtil.IProgressMonitor pm = new MonitorUtil.IProgressMonitor() {
+			private boolean cancelled;
 
-            @Override
-            public boolean isCanceled() {
-                if (System.currentTimeMillis() - startTime > clo.timeout) {
-                    this.cancelled = true;
-                }
-                return this.cancelled;
-            }
+			@Override
+			public void beginTask(final String s, final int i) {
 
-            @Override
-            public void done() {
+			}
 
-            }
+			@Override
+			public void subTask(final String s) {
 
-            @Override
-            public void worked(final int i) {
+			}
 
-            }
+			@Override
+			public void cancel() {
+				this.cancelled = true;
+			}
 
-            @Override
-            public String getCancelMessage() {
-                return "Timed out.";
-            }
-        };
-        return builder.makeCallGraph(options, pm);
-    }
+			@Override
+			public boolean isCanceled() {
+				if (System.currentTimeMillis() - startTime > clo.timeout) {
+					this.cancelled = true;
+				}
+				return this.cancelled;
+			}
+
+			@Override
+			public void done() {
+
+			}
+
+			@Override
+			public void worked(final int i) {
+
+			}
+
+			@Override
+			public String getCancelMessage() {
+				return "Timed out.";
+			}
+		};
+		return builder.makeCallGraph(options, pm);
+	}
 }
--- src/main/java/edu/utdallas/amordahl/CommandLineOptions.java
+++ src/main/java/edu/utdallas/amordahl/CommandLineOptions.java
@@ -1,102 +1,79 @@
 package edu.utdallas.amordahl;
 
 import com.ibm.wala.ipa.callgraph.AnalysisOptions.ReflectionOptions;
+
 import java.nio.file.Path;
+import java.util.List;
+
 import picocli.CommandLine.Option;
 
 public class CommandLineOptions {
-    @Option(names = "--jars", required = true, description = "The JARS to analyze, separated by :")
-    public String appJar;
+	@Option(names = "--jars", required = true, description = "The JARS to analyze, separated by :", split = ":")
+	public List<String> jars;
 
-    @Option(
-            names = {"--cgoutput", "-o"},
-            required = true,
-            description = "Where to write the callgraph.")
-    public Path callgraphOutput;
+	@Option(names = { "--cgoutput", "-o" }, required = true, description = "Where to write the callgraph.")
+	public Path callgraphOutput;
 
-    @Option(
-            names = "--reflectionSetting",
-            description = "Valid values: ${COMPLETION-CANDIDATES}",
-            defaultValue = "NONE")
-    public ReflectionOptions reflection;
+	@Option(names = "--reflectionSetting", description = "Valid values: ${COMPLETION-CANDIDATES}", defaultValue = "NONE")
+	public ReflectionOptions reflection;
 
-    // TODO: Rework config to allow direct setting of reflection options.
-  /*  @Option(names="--numFlowToCastIterations", description="How many times should flows from "
-      + "newInstance() calls to casts be analyzed?")
-  public int numFlowToCastIterations;
+	// TODO: Rework config to allow direct setting of reflection options.
+	/*
+	 * @Option(names="--numFlowToCastIterations",
+	 * description="How many times should flows from " +
+	 * "newInstance() calls to casts be analyzed?") public int
+	 * numFlowToCastIterations;
+	 * 
+	 * @Option(names="--ignoreMethodInoke",
+	 * description="should calls to Method.invoke() be ignored?") public boolean
+	 * ignoreMethodInvoke;
+	 * 
+	 * @Option(names="--applicationClassesOnly", description =
+	 * "should calls to Class.getMethod() " +
+	 * "be modeled only for application classes?") public boolean
+	 * applicationClassesOnly;
+	 * 
+	 * @Option(names="--ignoreStringConstants", description =
+	 * "should calls to reflective methods " +
+	 * "with string constant arguments be ignored?") public boolean
+	 * ignoreStringConstants;
+	 */
 
-  @Option(names="--ignoreMethodInoke", description="should calls to Method.invoke() be ignored?")
-  public boolean ignoreMethodInvoke;
+	@Option(names = { "-h", "--help" }, usageHelp = true, description = "display this help message")
+	boolean usageHelpRequested;
 
-  @Option(names="--applicationClassesOnly", description = "should calls to Class.getMethod() "
-      + "be modeled only for application classes?")
-  public boolean applicationClassesOnly;
+	@Option(names = { "--timeout", "-t" }, description = "Timeout in milliseconds.", defaultValue = "7200000")
+	public long timeout;
 
-  @Option(names="--ignoreStringConstants", description = "should calls to reflective methods "
-      + "with string constant arguments be ignored?")
-  public boolean ignoreStringConstants;*/
+	@Option(names = "--no-source-numbers", description = "Print out IR instead of source numbers.")
+	public boolean noSourceNumbers;
+	@Option(names = "--disableHandleStaticInit", description = "Should call graph construction handle "
+			+ "possible invocations of static initializer methods?")
+	public boolean disableHandleStaticInit;
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
-    boolean usageHelpRequested;
+	@Option(names = "--useConstantSpecificKeys", description = "Use distinct instance keys for "
+			+ "distinct string constants?")
+	public boolean useConstantSpecificKeys;
 
-    @Option(
-            names= {"--timeout", "-t"},
-            description ="Timeout in milliseconds.",
-            defaultValue = "7200000"
-    )
-    public long timeout;
+	@Option(names = "--useStacksForLexcialScoping", description = "Should analysis of lexical "
+			+ "scoping consider call stacks?")
+	public boolean useStacksForLexicalScoping;
 
-    @Option(
-            names = "--no-source-numbers",
-            description = "Print out IR instead of source numbers."
-    )
-    public boolean noSourceNumbers;
-    @Option(
-            names = "--disableHandleStaticInit",
-            description =
-                    "Should call graph construction handle "
-                            + "possible invocations of static initializer methods?")
-    public boolean disableHandleStaticInit;
+	@Option(names = "--useLexicalScopingForGlobals", description = "Should global variables be "
+			+ "considered lexically-scoped from the root node?")
+	public boolean useLexicalScopingForGlobals;
 
-    @Option(
-            names = "--useConstantSpecificKeys",
-            description = "Use distinct instance keys for " + "distinct string constants?")
-    public boolean useConstantSpecificKeys;
+	@Option(names = "--maxNumberOfNodes", description = "Maximum number of nodes that the "
+			+ "callgraph is allowed to have. -1 means no restrictions.", defaultValue = "-1")
+	public int maxNumberOfNodes;
 
-    @Option(
-            names = "--useStacksForLexcialScoping",
-            description = "Should analysis of lexical " + "scoping consider call stacks?")
-    public boolean useStacksForLexicalScoping;
+	@Option(names = "--disableHandleZeroLengthArray", description = "Should call graph construction handle "
+			+ "arrays of zero-length differently?")
+	public boolean disableHandleZeroLengthArray;
 
-    @Option(
-            names = "--useLexicalScopingForGlobals",
-            description =
-                    "Should global variables be " + "considered lexically-scoped from the root node?")
-    public boolean useLexicalScopingForGlobals;
+	@Option(names = "--cgalgo", description = "Valid values: ${COMPLETION-CANDIDATES}", defaultValue = "ZERO_CFA")
+	public CallGraphBuilders callGraphBuilder;
 
-    @Option(
-            names = "--maxNumberOfNodes",
-            description =
-                    "Maximum number of nodes that the "
-                            + "callgraph is allowed to have. -1 means no restrictions.",
-            defaultValue = "-1")
-    public int maxNumberOfNodes;
-
-    @Option(
-            names = "--disableHandleZeroLengthArray",
-            description = "Should call graph construction handle " + "arrays of zero-length differently?")
-    public boolean disableHandleZeroLengthArray;
-
-    @Option(
-            names = "--cgalgo",
-            description = "Valid values: ${COMPLETION-CANDIDATES}",
-            defaultValue = "ZERO_CFA")
-    public CallGraphBuilders callGraphBuilder;
-
-    @Option(
-            names = "--sensitivity",
-            description =
-                    "Level of context/object sensitivity (only used if cg algo is NCFA, NOBJ, VANILLA_NCFA, or VANILLA_NOBJ)",
-            defaultValue = "1")
-    public int sensitivity;
+	@Option(names = "--sensitivity", description = "Level of context/object sensitivity (only used if cg algo is NCFA, NOBJ, VANILLA_NCFA, or VANILLA_NOBJ)", defaultValue = "1")
+	public int sensitivity;
 }
